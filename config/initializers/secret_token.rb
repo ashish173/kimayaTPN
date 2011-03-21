150321eb1fcc8e1e329580388dc0953b7d5c0e76
Shailesh Patil <shailesh@joshsoftware.com>
2011-03-21 11:38:14 +0530

    Commiting the repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kimaya::Application.config.secret_token = 'c5e316bda94094bd0b1d97659d8e0b09e00e055f1f3cd0f9e10055f74e2af77f05ceddbbf338b087be08718dde4b59baab125dc91d49785287ae26e838282afc'
