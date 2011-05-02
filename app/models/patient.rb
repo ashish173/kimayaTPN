2c095002d53119fb5257eb05076eb7f043f5dd3b
Shailesh Patil <shailesh@joshsoftware.com>
2011-05-02 11:45:27 +0530

    Implemented the models for advance patient registration

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,19 +1,3 @@
-=begin
-t.integer :gender_id
-  t.string :name
-  t.integer :registration_number
-  t.datetime :date_of_birth
-  t.float :birth_weight
-  t.string :address
-  t.string :city
-  t.string :pincode
-  t.string :state
-  t.string :residence_telephone
-  t.string :emergency_telephone
-  t.string :mobile_number
-  t.string :additional_detail
-  t.timestamps
-=end
 class Patient < ActiveRecord::Base
  validates_presence_of :name, :message => :patient_name_blank
  validates_presence_of :date_of_birth, :message => :date_of_birth_blank
@@ -23,4 +7,7 @@ class Patient < ActiveRecord::Base
  validates_format_of  :emergency_telephone, :with =>  /^\d{3}-\d{8}$/, :message => :phone_number_invalid
  validates_format_of  :residence_telephone, :with =>  /^\d{3}-\d{8}$/, :allow_blank => true, :message => :phone_number_invalid
  validates_format_of :mobile_number, :with =>  /^\d{10}/, :allow_blank => true, :message => :mobile_number_invalid
+
+ has_one :mother_history, :depedent => :destory
+ has_one :patient_history, :depedent => :destory
 end
