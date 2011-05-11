9bbc05c2fe7136e86f0ac4637aac46cdc43759d3
Shailesh Patil <shailesh@joshsoftware.com>
2011-05-11 15:30:27 +0530

    changing the style of the inputs

--- db/migrate/20110509113233_add_ga_to_patient_history.rb
+++
@@ -1,9 +0,0 @@
-class AddGaToPatientHistory < ActiveRecord::Migration
-  def self.up
-    add_column :patient_histories, :ga, :string
-  end
-
-  def self.down
-    remove_column :patient_histories, :ga
-  end
-end
