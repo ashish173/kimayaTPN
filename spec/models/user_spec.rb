e8237154c885e832ecbe60b4cbb70d2f73dde8ae
ashish173 <aitashish173@gmail.com>
2013-07-08 15:43:52 +0530

    wrote user model specs

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -16,33 +16,5 @@ describe User do
       email: "aitashish173@gmail.com")  
     expect(user).to have(1).errors_on(:role_id)  
   end
-=begin
-  it "is invalid without a hospital_id" do
-    user = User.new(
-      name: "ashish singh",
-      email: "aitashish173@gmail.com",
-      )
-    expect(user).to have(1).errors_on(:hospital_id)  
-  end
-=end  
-  it "is invalid with already used email address" do
-    # its not allowing to create a record 
-    # as in the background on user create it tries to 
-    # send an email and smtp connection is not being 
-    # estabilished for some reason.
-=begin    user1 = User.create(
-      name: "ashish singh",
-      email: "aitashish173@gmail.com",
-      password: "fedora",
-      role_id: 2)
-    user = User.new(
-      name: "c0mrade",
-      email: "aitashish173@gmail.com")  
-      expect(user).to have(1).errors_on(:email)  
-=end      
-  end
   
-   
-
-
 end
