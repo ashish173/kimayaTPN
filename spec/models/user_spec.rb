2616eece1a3a12e3bbbc81fefe2281f55f7f9b5c
ashish173 <aitashish173@gmail.com>
2013-07-09 01:38:38 +0530

    added more specs in user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,47 +1,37 @@
 require 'spec_helper'
 
 describe User do
+  let(:user) { User.new(name: "test user", email: "testuser@gmail.com", password: "testpass", role_id: 1) }
+
   it "is invalid without a name" do
-    user = User.new(
-      address: "vassar street",
-      email: "aitashish173@gmail.com",
-      role_id: 2)
+    user.name = nil
     expect(user).to have(1).errors_on(:name)
-    #expect(user).to have(1).errors_on(:role_id)
   end
 
   it "is invalid without a role_id" do
-    user = User.new(  
-      name: "ashish singh",
-      email: "aitashish173@gmail.com")  
+    user.role_id = nil
     expect(user).to have(1).errors_on(:role_id)  
   end
-=begin
-  it "is invalid without a hospital_id" do
-    user = User.new(
-      name: "ashish singh",
-      email: "aitashish173@gmail.com",
-      )
-    expect(user).to have(1).errors_on(:hospital_id)  
-  end
-=end  
-  it "is invalid with already used email address" do
-    # its not allowing to create a record 
-    # as in the background on user create it tries to 
-    # send an email and smtp connection is not being 
-    # estabilished for some reason.
-    user1 = User.create!(
-      name: "ashish singh",
-      email: "aitashish173@gmail.com",
-      password: "fedora",
-      role_id: 2)
-    user = User.new(
-      name: "c0mrade",
-      email: "aitashish173@gmail.com")  
-      expect(user).to have(1).errors_on(:email)  
-  end
-  
-   
 
+  it "is invalid with already used email address" do
+    user.save  # persist the object
+    user1 = User.new(name: "c0mrade", email: "testuser@gmail.com")  
+    expect(user1).to have(1).errors_on(:email)
+  end
+ 
+  it "is invalid if email address not present" do
+    user.email = nil
+    expect(user).to have(1).errors_on(:email)
+  end
+   
+  it "is invalid if the password is not given" do
+    user.password = nil
+    expect(user).to have(1).errors_on(:password)
+  end
+
+  it "is invalid if role_id not given " do
+    user.role_id = nil
+    expect(user).to have(1).errors_on(:role_id)
+  end
 
 end
