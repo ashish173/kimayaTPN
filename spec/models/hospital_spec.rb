35c67e4371ec49a24e8b38fa71aa425c25f66d80
khushnaseeb <khushnaseeb14@yahoo.co.in>
2013-07-09 00:08:23 +0530

    cleaned hospital spec

--- spec/models/hospital_spec.rb
+++ spec/models/hospital_spec.rb
@@ -14,21 +14,10 @@ describe Hospital do
 
   it "should have patients within its limit if specified" do 
     hospital.patients << patient
-    #hospital.patients.create(gender_id: "24", name:"jane", registration_number:"11", date_of_birth:Date.today, birth_weight:5,
-     #                       address:"pune",hospital_id:2)
-    
-     patient=hospital.patients.build(gender_id:"24", name: "jane", registration_number: "60", date_of_birth: "2013-07-4", birth_weight: 5,
-                                         address:"pune")
-       expect(hospital).to_not be_valid
-       p patient.errors.full_messages
-
-    #expect(hospital).to_not be_valid
-
-
-     
+    patient=hospital.patients.build(gender_id:"24", name: "jane", registration_number: "60", date_of_birth: "2013-07-4", birth_weight: 5,
+                                    address:"pune")
+    expect(hospital).to_not be_valid
+    p patient.errors.full_messages
   end
 
-  it "should have patients"
-
-  it "should have users"
 end
