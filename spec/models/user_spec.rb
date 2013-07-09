ed9f34a4e3a8b2e0a23b7646e96f80e9ff20af75
ashish173 <aitashish173@gmail.com>
2013-07-09 15:30:07 +0530

    added user spec using factory

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,37 +1,34 @@
 require 'spec_helper'
 
 describe User do
-  let(:user) { User.new(name: "test user", email: "testuser@gmail.com", password: "testpass", role_id: 1) }
+  # let(:user) { User.new(name: "test user", email: "testuser@gmail.com", password: "testpass", role_id: 1) }
 
   it "is invalid without a name" do
-    user.name = nil
-    expect(user).to have(1).errors_on(:name)
+    expect(build(:user, name: nil)).to have(1).errors_on(:name)
   end
 
   it "is invalid without a role_id" do
-    user.role_id = nil
-    expect(user).to have(1).errors_on(:role_id)  
+    
+    expect(build(:user, role_id: nil)).to have(1).errors_on(:role_id)  
   end
 
   it "is invalid with already used email address" do
-    user.save  # persist the object
-    user1 = User.new(name: "c0mrade", email: "testuser@gmail.com")  
-    expect(user1).to have(1).errors_on(:email)
+    create(:user, email: "ashish@gmail.com")
+    expect(build(:user, email: "ashish@gmail.com")).to have(1).errors_on(:email)
   end
  
   it "is invalid if email address not present" do
-    user.email = nil
-    expect(user).to have(1).errors_on(:email)
+    expect(build(:user, email: nil)).to have(1).errors_on(:email)
   end
    
   it "is invalid if the password is not given" do
-    user.password = nil
-    expect(user).to have(1).errors_on(:password)
+    
+    expect(build(:user, password: nil)).to have(1).errors_on(:password)
   end
 
   it "is invalid if role_id not given " do
-    user.role_id = nil
-    expect(user).to have(1).errors_on(:role_id)
+    expect(build(:user, role_id: nil)).to have(1).errors_on(:role_id)
   end
-
+  
 end
+
