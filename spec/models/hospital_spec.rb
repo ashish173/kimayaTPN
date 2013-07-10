68ff518de837cfeebf1c388bf593a78b35fbb9ee
khushnaseeb <khushnaseeb14@yahoo.co.in>
2013-07-10 13:37:36 +0530

    specs

--- spec/models/hospital_spec.rb
+++ spec/models/hospital_spec.rb
@@ -1,11 +1,7 @@
 require 'spec_helper'
 
 describe Hospital do
-  
- # let(:hospital){Hospital.create(name:"ruby",patients_count:1)}
- # let(:patient){Patient.create(gender_id: "24", name:"jill", registration_number:"101", date_of_birth:Date.today, birth_weight:5,
-                               #address:"pune",hospital_id:2)}
-  
+   
   it "should have a unique name" do
     create(:hospital, name: "ruby")
     expect(build(:hospital, name: "ruby")).to have(1).errors_on(:name)
