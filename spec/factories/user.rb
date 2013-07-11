7cce18770633285ef89957fa022078e67aaf28fa
khushnaseeb <khushnaseeb14@yahoo.co.in>
2013-07-11 11:20:40 +0530

    removed duplicates

--- spec/factories/user.rb
+++
@@ -1,8 +0,0 @@
-FactoryGirl.define do
-  factory :user do
-    sequence(:name) { |n| "#{n}singh" }
-    role_id 2
-    password "somepassword"
-    sequence(:email) {|email| "#{email}myemail@gmail.com" }
-  end
-end
